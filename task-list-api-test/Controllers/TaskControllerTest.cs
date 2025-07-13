fe6661ca2ba2b0ec72b34ba09f719359d486e4dc
nxhuel <nahueltisera03@gmail.com>
2025-07-12 21:21:57 -0300

    task-list-api: integracion unit test

---
+++ task-list-api-test/Controllers/TaskControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using task_list_api.Controllers;
+using task_list_api.Dtos;
+using task_list_api.Models;
+using task_list_api.Services.Impl;
+using task_list_api.Utils;
+
+namespace task_list_api_test.Controllers
+{
+    [TestClass]
+    public sealed class TaskControllerTest : TestBase
+    {
+        [TestMethod]
+        public async Task Get_GetTasks()
+        {
+            // Preparacion
+            var nameDb = Guid.NewGuid().ToString();
+            var context = Context(nameDb);
+            var mapper = ConfigureAutoMapper();
+
+            context.TaskModels.Add(new TaskModel()
+            {
+                Title = "Task 1000",
+                Priority = "medium",
+                Tags = new string[] { "urgent" }
+            });
+            context.TaskModels.Add(new TaskModel()
+            {
+                Title = "Task 2000",
+                Priority = "medium",
+                Tags = new string[] { "normal" }
+            });
+
+            await context.SaveChangesAsync();
+
+            var context2 = Context(nameDb);
+            var service = new TaskServiceImpl(context2, mapper);
+            var controller = new TaskController(service);
+
+            // ejecucion
+            var response = await controller.GetTasks();
+
+            // verificacion
+            var result = response.Result as OkObjectResult;
+            var pagedResult = result?.Value as PagedResult<TaskDto>;
+
+            Assert.IsNotNull(pagedResult);
+            Assert.AreEqual(2, pagedResult.Items.Count);
+        }
+
+    }
+}
--- task-list-api/obj/Debug/net8.0/task-list-api.AssemblyInfo.cs
+++ task-list-api/obj/Debug/net8.0/task-list-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task-list-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2cc309f0dfb4878543697128bcd919233a2351e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de165a2370c99f49a52401e061f3bb0818a2e66")]
 [assembly: System.Reflection.AssemblyProductAttribute("task-list-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task-list-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
